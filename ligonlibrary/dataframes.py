35389fb902155caf5573ac2798f4d8d5faecde63
Ethan Ligon <ligon@berkeley.edu>
2025-08-18 15:42:42 -0700

    Add get_dataframe to dataframes.py.

--- ligonlibrary/dataframes.py
+++ ligonlibrary/dataframes.py
@@ -2,9 +2,107 @@
 
 """Miscellany of tools for manipulating dataframes.
 """
-
-from . import strings
 import pandas as pd
+import dvc.api
+from . import strings
+from pyarrow.lib import ArrowInvalid
+from functools import lru_cache
+from pathlib import Path
+from cfe.df_utils import df_to_orgtbl
+from importlib.resources import files
+from dvc.api import DVCFileSystem
+from lsms import from_dta
+import pyreadstat
+
+@lru_cache(maxsize=3)
+def get_dataframe(fn,convert_categoricals=True,encoding=None,categories_only=False):
+    """From a file named fn, try to return a dataframe.
+
+    Hope is that caller can be agnostic about file type,
+    or if file is local or on a dvc remote.
+    """
+
+    def local_file(fn):
+    # Is the file local?
+        try:
+            with open(fn) as f:
+                pass
+            return True
+        except FileNotFoundError:
+            return False
+
+    def file_system_path(fn):
+    # is the file a relative path or it's the full path from our fs (DVCFileSystem)?
+        try:
+            with DVCFS.open(fn) as f:
+                pass
+            return True
+        except FileNotFoundError:
+            return False
+
+    def read_file(f,convert_categoricals=convert_categoricals,encoding=encoding):
+        if isinstance(f,str):
+            try:
+                return pd.read_spss(f,convert_categoricals=convert_categoricals)
+            except (pd.errors.ParserError, UnicodeDecodeError):
+                pass
+
+        try:
+            return pd.read_parquet(f, engine='pyarrow')
+        except (ArrowInvalid,):
+            pass
+
+        try:
+            f.seek(0)
+            return from_dta(f,convert_categoricals=convert_categoricals,encoding=encoding,categories_only=categories_only)
+        except ValueError:
+            pass
+
+        try:
+            f.seek(0)
+            return pd.read_csv(f,encoding=encoding)
+        except (pd.errors.ParserError, UnicodeDecodeError):
+            pass
+
+        try:
+            f.seek(0)
+            return pd.read_excel(f)
+        except (pd.errors.ParserError, UnicodeDecodeError, ValueError):
+            pass
+
+        try:
+            f.seek(0)
+            return pd.read_feather(f)
+        except (pd.errors.ParserError, UnicodeDecodeError,ArrowInvalid) as e:
+            pass
+
+        try:
+            f.seek(0)
+            return pd.read_fwf(f)
+        except (pd.errors.ParserError, UnicodeDecodeError):
+            pass
+
+
+        raise ValueError(f"Unknown file type for {fn}.")
+
+    if local_file(fn):
+        try:
+            with open(fn,mode='rb') as f:
+                df = read_file(f,convert_categoricals=convert_categoricals,encoding=encoding)
+        except (TypeError,ValueError): # Needs filename?
+            df = read_file(fn,convert_categoricals=convert_categoricals,encoding=encoding)
+    elif file_system_path(fn):
+        try:
+            with DVCFS.open(fn,mode='rb') as f:
+                df = read_file(f,convert_categoricals=convert_categoricals,encoding=encoding)
+        except TypeError: # Needs filename?
+            df = read_file(fn,convert_categoricals=convert_categoricals,encoding=encoding)
+
+    else:
+        with dvc.api.open(fn,mode='rb') as f:
+            df = read_file(f,convert_categoricals=convert_categoricals,encoding=encoding)
+
+    return df
 
 def normalize_strings(df,**kwargs):
     """Normalize strings in a dataframe.
